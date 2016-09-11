9f5f35d4f453e84bf45360143650526a9c394fe8
Bryan Owens <djbryan3540@gmail.com>
2016-09-12 00:10:25 +0800

    Themes: Expose hard coded icon tint in power summary settings (original commit http://review.cyanogenmod.org/#/c/136136/ by @djdarkknight96)

--- src/com/android/settings/fuelgauge/PowerUsageDetail.java
+++ src/com/android/settings/fuelgauge/PowerUsageDetail.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2016 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -486,7 +486,7 @@ public class PowerUsageDetail extends PowerUsageBase implements Button.OnClickLi
             pkg = mPackages[0];
         }
         AppHeader.createAppHeader(this, appIcon, title, pkg, uid,
-                mDrainType != DrainType.APP ? android.R.color.white : 0);
+                mDrainType != DrainType.APP ? R.color.power_usage_ab_icon_tint : 0);
     }
 
     public void onClick(View v) {
